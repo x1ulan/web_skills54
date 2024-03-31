36bf4cd8c2c690702ecbbbb5d3626d77f6e772a0
x1ulan <ookun@gmail.com>
2024-03-31 09:18:35 +0800

    add refresh to resolve cookie problem

--- web_3/panel/index.php
+++ web_3/panel/index.php
@@ -6,12 +6,14 @@ if (!isset($_SESSION["username"])){
 }
 if (!isset($_COOKIE["is_admin"])){
     setcookie("is_admin",base64_encode("no"),time()+3600, "/");
+    header('refresh:0');
 }
 if (!isset($_COOKIE["username"])){
     setcookie("username",base64_encode($_SESSION["username"]),time()+3600, "/");
+    header('refresh:0');
 }
 if (base64_decode(urldecode($_COOKIE["is_admin"]))==="yes"){
-    if (base64_decode(urldecode($_COOKIE["username"]))==='seadog007'){
+    if (base64_decode(urldecode($_COOKIE["username"]))==="seadog007"){
         echo "Hello admin seadog007<br>here is your flag<br>skill54{s3ad0g007}";
     }else{
         echo "you are admin<br>but I only give flag to seadog007";
